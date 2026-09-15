e55efd7383dfd7cb7c07bb77dc9b9a6fe83f3b5a
Skeesler5915 <skeesler5915@gmail.com>
2026-09-14 18:14:51 -0700

    Add bit inversion logic to invert7 module
    Implemented basic functionality to invert input bits.

--- skel/invert7.v
+++ skel/invert7.v
@@ -4,8 +4,10 @@ module invert7(
     input [6:0] a,
     output [6:0] x  
     );
-//
-// fill in the rest here
-//   
+
+    always
+        begin
+        output = ~input;
+        case
 endmodule
 
