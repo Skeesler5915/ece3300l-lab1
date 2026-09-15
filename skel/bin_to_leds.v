a6b0b0cb3211933715b388dbf7c6631052174c83
Skeesler5915 <skeesler5915@gmail.com>
2026-09-14 18:00:14 -0700

    Update bin_to_leds.v

--- skel/bin_to_leds.v
+++ skel/bin_to_leds.v
@@ -8,6 +8,15 @@ module bin_to_leds(
     always @(b_in)
         case(b_in)
             0: leds = 7'b0111111;
+            1: leds = 7'b;
+            2: leds = 7'b;
+            3: leds = 7'b;
+            4: leds = 7'b;
+            5: leds = 7'b;
+            6: leds = 7'b;
+            7: leds = 7'b;
+            8: leds = 7'b;
+            9: leds = 7'b;
 //
 // fill in the rest here
 //
